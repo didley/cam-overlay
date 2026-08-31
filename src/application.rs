f86e3af417cf23ef64d8da24542344a5603773d9
Dylan Lamont <didley.dev@gmail.com>
2026-08-31 12:38:07 +1000

    Tag window title for PiP on top extension compatibility
    Sets the window title to end in " - PiP" so the GNOME "PiP on Top"
    extension automatically keeps Cam Overlay above other windows, since
    GNOME's Wayland session doesn't expose a native always-on-top option.
    Also mentions the extension in the README and About dialog.

--- src/application.rs
+++ src/application.rs
@@ -97,7 +97,7 @@ impl CamOverlayApplication {
             content.append(&heading);
 
             let body = gtk4::Label::new(None);
-            body.set_markup("Webcam preview overlay for screen recording.\n\nCreated for fun by @didley, with love from Melbourne.\n\n<b>Always on Top</b>\nUse your compositor's window menu to set always on top.\nOn GNOME: Super + Right Click on the window, or Alt + Space.\n\n<b>Full Screen</b>\nDouble left-click the overlay to toggle full screen.\n\nLicense: GPL-3.0-or-later\n<a href=\"https://github.com/didley/camoverlay\">github.com/didley/camoverlay</a>");
+            body.set_markup("Webcam preview overlay for screen recording.\n\nCreated for fun by @didley, with love from Melbourne.\n\n<b>Always on Top</b>\nUse your compositor's window menu to set always on top.\nOn GNOME: Super + Right Click on the window, or Alt + Space.\nOr a great extension for this is <a href=\"https://extensions.gnome.org/extension/4691/pip-on-top/\">PiP on Top</a>.\n\n<b>Full Screen</b>\nDouble left-click the overlay to toggle full screen.\n\nLicense: GPL-3.0-or-later\n<a href=\"https://github.com/didley/camoverlay\">github.com/didley/camoverlay</a>");
             body.set_xalign(0.0);
             body.set_wrap(true);
             content.append(&body);
